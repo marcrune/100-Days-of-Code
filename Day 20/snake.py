0e5efa42e2f5fb595ef91204b27713ea5d0b42ce
Marcrune <marcoferrini@hotmail.com>
2024-07-23 17:40:07 -0300

    finished day 20 using OOP to build the snake game

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,5 +1,10 @@
 from turtle import Turtle
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 # constant to facilitate changing the code if necessary or desired
 MOVE_DISTANCE = 20
 
@@ -8,8 +13,9 @@ class Snake():
         self.segments = []
         self.create_snake()
 
+
     def create_snake(self):
-        """creates a snake with 3 segments of size 20 and color white"""
+        """creates a snake with 3 segments of size 20, color white and square shaped"""
         for n in range(3):
             snake = Turtle(shape='square')
             snake.fillcolor('white')
@@ -24,3 +30,24 @@ class Snake():
         for n in range(len(self.segments)-1, 0, -1):
             self.segments[n].goto(self.segments[n-1].pos())
         self.segments[0].forward(MOVE_DISTANCE)
+
+
+    def up(self):
+        """moves the snake upwards (towards 90 degrees)"""
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].seth(UP)
+
+    def down(self):
+        """moves the snake downwards (towards 270 degrees)"""
+        if self.segments[0].heading() != UP:
+            self.segments[0].seth(DOWN)
+
+    def right(self):
+        """moves the snake right (towards 0/360 degrees)"""
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].seth(RIGHT)
+
+    def left(self):
+        """moves the snake left (towards 180 degrees)"""
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].seth(LEFT)
