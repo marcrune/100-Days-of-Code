3c610027b8d80357434e1362f869772f49ffa90c
Marcrune <marcoferrini@hotmail.com>
2024-09-03 23:50:42 -0300

    Finished day 25 (US states game) using the pandas and turtle libraries as well as file handling

--- Day 25/us_states_game.py
+++ Day 25/us_states_game.py
@@ -24,22 +24,28 @@ while game_is_on:
         answer_state = screen.textinput(title=f"{len(correct_answers)}/50 states correct", prompt="What's another state's name")
         answer_state = answer_state.title()
 
-    # Checking to see if user answer is correct and then writing the name on the state
-    for state in data["state"]:
-        if answer_state in correct_answers:
-            continue
-        elif answer_state == state:
-            name_on_map = turtle.Turtle()
-            name_on_map.hideturtle()
-            get_answer = data[data["state"] == answer_state]
-            name_on_map.teleport(x=data.iloc[get_answer.index.item()]["x"], y=data.iloc[get_answer.index.item()]["y"])
-            name_on_map.write(answer_state)
-            correct_answers.append(answer_state)
-    
+    # Checking user answer
+    if answer_state in correct_answers:
+        continue
+    elif answer_state in data["state"].values:
+        name_on_map = turtle.Turtle()
+        name_on_map.hideturtle()
+        name_on_map.penup()
+        get_answer = data[data["state"] == answer_state]
+        name_on_map.teleport(x=data.iloc[get_answer.index.item()]["x"], y=data.iloc[get_answer.index.item()]["y"])
+        name_on_map.write(answer_state, align="center")
+        correct_answers.append(answer_state)
+    elif answer_state == "Exit":
+        with open("states_to_learn.csv", mode="w") as file:
+            for state in data["state"].values:
+                if state not in correct_answers:
+                    file.write(f"{state}\n")
+        game_is_on = False
+
     # Game over sequence
     if len(correct_answers) == 50:
         game_is_on = False
-        print("You win!")
-
-
-screen.exitonclick()
+        name_on_map.clear()
+        game_over = turtle.Turtle()
+        game_over.hideturtle()
+        game_over.write("YOU WIN!", align="center", font=("Arial", 50, "bold"))
