a7393a94077f2d7ab14872ddc79a357884aa8e5c
Marcrune <marcoferrini@hotmail.com>
2024-11-05 22:37:48 -0300

    Started creating the turn_card function but haven't finished

--- Day 31/main.py
+++ Day 31/main.py
@@ -15,8 +15,9 @@ data_dict = data.to_dict(orient="records")
 # -------------------------------------- UI CONFIG --------------------------------------
 # Canvas
 canvas = tkinter.Canvas(width=800, height=526, highlightthickness=0, background=BACKGROUND_COLOR)
-logo_img = tkinter.PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=logo_img)
+front_img = tkinter.PhotoImage(file="./images/card_front.png")
+back_image = tkinter.PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=front_img)
 card_title = canvas.create_text(400, 150, text=data.columns[0], font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text=choice(data["French"]), font=("Ariel", 40, "bold"))
 
@@ -25,8 +26,15 @@ canvas.grid(column=0, row=0, columnspan=2)
 # Random word function
 def random_word():
     """Picks a random french word and puts it into the canvas."""
-    word = choice(data["French"])
+    word = choice(data_dict)["French"]
     canvas.itemconfig(card_word, text=word)
+
+# Turn card function
+def turn_card():
+    """Turns the card showing the english word after 3 seconds."""
+    canvas.itemconfig(canvas_image, image=back_image)
+    canvas.itemconfig(card_title, text=data.columns[1], fill="white")
+    # canvas.itemconfig(card_word, text=, color="white")
 
 # Buttons
 right_image = tkinter.PhotoImage(file="./images/right.png")
@@ -37,4 +45,10 @@ wrong_image = tkinter.PhotoImage(file="./images/wrong.png")
 wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=random_word)
 wrong_button.grid(column=0, row=1)
 
-window.mainloop()
+window.after(3000, turn_card)
+
+# print(data.iloc[0].values[1])
+# print(data.index[data["French"] == "partie"].values[0])
+print(data.iloc[data.index[data["French"] == card_word]])
+
+# window.mainloop()
